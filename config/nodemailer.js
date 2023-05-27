41b1729e31f68f3a00863ead520bdce39ff563de
Abhishek2407200 <118927569+Abhishek2407200@users.noreply.github.com>
2023-05-28 03:34:31 +0530

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -9,8 +9,8 @@ let transporter  = nodemailer.createTransport({
     port:587,
     secure:false,
     auth:{
-        user:'abishak22221@gmail.com',
-        pass:'njfscvoabcywfyxy'
+        user:'examle@gmail.com',
+        pass:'passwordexample'
     }
 });
 
